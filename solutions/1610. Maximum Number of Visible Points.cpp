7b8a76937393e7fb610c580014fa58d221c79552
Ishu Dharmendra Garg <ishugarg567@gmail.com>
2021-04-11 17:11:58 +0530

    416 ms

--- solutions/1610. Maximum Number of Visible Points.cpp
+++ solutions/1610. Maximum Number of Visible Points.cpp
@@ -1,3 +1,23 @@
+double PI = 180 / (2 * acos(0.0));
+​
+class Solution {
+    double getAngle(int x, int y) {
+        double angle = atan2(y, x) * PI;
+        if(angle < 0)
+            angle += 360;
+        return angle;
+    }
+    
+public:
+    int visiblePoints(vector<vector<int>>& points, int angle, vector<int>& location) {
+        if(angle < 0)
+            angle = -angle;
+        vector<double> newPoints;
+        int addExtra = 0;
+        for(int i = 0; i < points.size(); i++) {
+            int xp = points[i][0], yp = points[i][1];
+            if(xp == location[0] && yp == location[1])
+                addExtra++;
             else {
                 newPoints.push_back(getAngle(xp - location[0], yp - location[1]));
             }
@@ -5,26 +25,5 @@
         sort(newPoints.begin(), newPoints.end());
         int newPointSize = newPoints.size();
         double startAngle = 0, endAngle = -angle;
-        int idx = 0, maxPoints = 0, backPtr = 0;;
-        while(idx < newPoints.size()) {
-            startAngle = newPoints[idx];
-            endAngle = startAngle - angle;
-            while(endAngle > newPoints[backPtr]) {
-                backPtr++;
-            }
-            idx++;
-            maxPoints = max(maxPoints, idx - backPtr);
-        }
-        while(backPtr < newPoints.size()) {
-            int idxUsed = idx % (int) newPoints.size();
-            startAngle = newPoints[idxUsed] + 360;
-            endAngle = startAngle - angle;
-            while(backPtr < newPoints.size() && endAngle > newPoints[backPtr]) {
-                backPtr++;
-            }
-            idx++;
-            maxPoints = max(maxPoints, idx - backPtr);
-        }
-        return maxPoints + addExtra;
-    }
-};
+        int idx = 0, maxPoints = 0, backPtr = 0, N = (int) newPoints.size() ;
+        
