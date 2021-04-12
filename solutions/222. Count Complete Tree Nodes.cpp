a91622730f5fa3a127c0a66b516419a6a7282050
Ishu Dharmendra Garg <ishugarg567@gmail.com>
2021-04-12 12:09:15 +0530

    24 ms

--- solutions/222. Count Complete Tree Nodes.cpp
+++ solutions/222. Count Complete Tree Nodes.cpp
@@ -1,15 +1,10 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
+    map<int, int> twoPower;
+​
     int getLeftHieght(TreeNode* root) {
         if(root->left == NULL)
             return 0;
@@ -23,19 +18,32 @@ class Solution {
         else
             return 1 + getRightHieght(root->right);
     }
-    map<int, int> twoPower;
     
-public:
-    int countNodes(TreeNode* root) {
+    int countNodes(TreeNode* root, int leftHieght, int rightHieght) {
         if(root == NULL)
             return 0;
-        int leftHieght = getLeftHieght(root), rightHieght = getRightHieght(root);
-        if(leftHieght == rightHieght) {
+        else if(root->left == NULL)
+            return 1;
+        else if(root->right == NULL)
+            return 2;
+        else if(leftHieght == rightHieght) {
             if(twoPower.find(leftHieght + 1) == twoPower.end())
                 twoPower[leftHieght + 1] = pow(2, leftHieght + 1) - 1;
             return twoPower[leftHieght + 1];
         }
         else 
-            return (1 + countNodes(root->left) + countNodes(root->right));
+            return (1 + countNodes(root->left, leftHieght - 1, getRightHieght(root->left)) + countNodes(root->right, getLeftHieght(root->right), rightHieght - 1));
+    }
+​
+public:
+    int countNodes(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        else if(root->left == NULL)
+            return 1;
+        else if(root->right == NULL)
+            return 2;
+        else 
+            return countNodes(root, getLeftHieght(root), getRightHieght(root));
     }
 };
