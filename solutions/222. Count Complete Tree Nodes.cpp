811942d06e0ff186665fb9d0e656cd8a5683329c
Ishu Dharmendra Garg <ishugarg567@gmail.com>
2021-04-12 11:28:45 +0530

    32 ms

---
+++ solutions/222. Count Complete Tree Nodes.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int getLeftHieght(TreeNode* root) {
+        if(root->left == NULL)
+            return 0;
+        else
+            return 1 + getLeftHieght(root->left);
+    }
+    
+    int getRightHieght(TreeNode* root) {
+        if(root->right == NULL)
+            return 0;
+        else
+            return 1 + getRightHieght(root->right);
+    }
+​
+public:
+    int countNodes(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        int leftHieght = getLeftHieght(root), rightHieght = getRightHieght(root);
+        if(leftHieght == rightHieght)
+            return (pow(2, leftHieght + 1) - 1);
+        else 
+            return (1 + countNodes(root->left) + countNodes(root->right));
+    }
+};
