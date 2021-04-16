daf4f45b3df82b6f5d640808392e10df3b5bb81f
Ishu Dharmendra Garg <ishugarg567@gmail.com>
2021-04-16 11:16:06 +0530

    0 ms

--- solutions/1041. Robot Bounded In Circle.cpp
+++ solutions/1041. Robot Bounded In Circle.cpp
@@ -1,32 +1,37 @@
 class Solution {
     map<int, pair<int , int> > dirInc = {{0, {0, 1}}, {1, {1, 0}}, {2, {0, -1}}, {3, {-1, 0}}};
     
-    int updateDir(int dir, char c) {
-        if(c == 'R')
-            ++dir;
-        else {
-            --dir;
-        }
-        return (dir + 4) % 4;
-    }
+    // int updateDir(int dir, char c) {
+    //     if(c == 'R')
+    //         ++dir;
+    //     else {
+    //         --dir;
+    //     }
+    //     return (dir + 4) % 4;
+    // }
     
 public:
     bool isRobotBounded(string instructions) {
         int x = 0, y = 0;
         int dir = 0;
-        for(int i = 0; i < 4; i++) {
-            for(char c : instructions) {
-                if(c == 'G') {
-                    x += dirInc[dir].first;
-                    y += dirInc[dir].second;
-                } else {
-                    dir = updateDir(dir, c);
-                }
+        
+        for(char c : instructions) {
+            if(c == 'G') {
+                x += dirInc[dir].first;
+                y += dirInc[dir].second;
+            } else {
+                if(c == 'L')
+                    dir++;
+                else
+                    dir--;
+                dir += 4;
+                dir %= 4;
+                // dir = updateDir(dir, c);
             }
         }
-        if(x != 0 || y != 0)
-            return false;
-        else
+        if((x == 0 and y == 0) or (dir != 0))
             return true;
+        else
+            return false;
     }
 };
