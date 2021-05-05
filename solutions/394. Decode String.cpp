386ce2924926e937c4fe610114769d0107c82478
Ishu Dharmendra Garg <ishugarg567@gmail.com>
2021-05-05 16:53:24 +0530

    0 ms

--- solutions/394. Decode String.cpp
+++ solutions/394. Decode String.cpp
@@ -1,37 +1,40 @@
 class Solution {
-    string decodeStringHelper(string &s, int depth, int& nextScan) {
-        // cout << nextScan << endl;
-        if(nextScan >= s.size())
-            return "";
-        string repString = "", headString = "";
-        int j = nextScan;
-        while(j < s.size() && isalpha(s[j]))
-            j++;
-        headString = s.substr(nextScan, j - nextScan);
-        nextScan = j;
-        while(j < s.size() && isdigit(s[j]))
-            j++;
-        if(nextScan != j) {
-            int rep = stoi(s.substr(nextScan, j - nextScan));
-            nextScan = j+1;
-            string currString = "";
-            while(nextScan < s.size() && s[nextScan] != ']')
-                currString += decodeStringHelper(s, depth + 1, nextScan);
-            while(rep--)
-                repString += currString;
-            nextScan++; // for eliminating the ]
+    string repStr(string s, int n) {
+        string ans = "";
+        while(n--) {
+            ans += s;
         }
-        headString = headString +  repString;
-        // cout << headString << endl;
-        if(depth == 0)
-            return headString + decodeStringHelper(s, depth, nextScan);
-        else
-            return headString;
+        return ans;
     }
-    
 public:
     string decodeString(string s) {
-        int nextScan = 0;
-        return decodeStringHelper(s, 0, nextScan);
+        stack< pair<int, string> > pile;
+        string repS = "";
+        int rep = 1;
+        //pile.push({rep, repS});
+        int i = 0;
+        while(i < s.size()) {
+            if(s[i] == '[') {
+                i++;
+            } else if(s[i] == ']') {
+                auto pileHead = pile.top();
+                pile.pop();
+                repS = pileHead.second + repStr(repS, rep);
+                rep = pileHead.first;
+                i++;
+            } else if(isalpha(s[i])) {
+                repS += s[i];
+                i++;
+            } else {
+                pile.push({rep, repS});
+                int j = i;
+                while(j < s.size() && isdigit(s[j]))
+                    j++;
+                rep = stoi(s.substr(i, j - i));
+                repS = "";
+                i = j;
+            }
+        }
+        return repS;
     }
 };
