3022f0d8b81eff13cf271528d5ffa31c1cf4a7e9
Ishu Dharmendra Garg <ishugarg567@gmail.com>
2021-06-23 00:11:53 +0530

    4 ms

--- solutions/833. Find And Replace in String.cpp
+++ solutions/833. Find And Replace in String.cpp
@@ -2,28 +2,26 @@ class Solution {
 //     assuming indices are in sorted order
 public:
     string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
-        unordered_map<int, int> indMapping;
+        map<int, int> indMapping;
         for(int i = 0; i < indices.size(); i++) {
             indMapping[indices[i]] = i;
         }
         sort(indices.begin(), indices.end());
         string ans;
         int i = 0, j = 0;
-        while(i < s.size()) {
-            if(j < indices.size() and i == indices[j]) {
+        while(i < s.size() and j < indices.size()) {
+            if(i == indices[j]) {
                 if(s.substr(i, sources[indMapping[indices[j]]].size()) == sources[indMapping[indices[j]]]) {
                     ans += targets[indMapping[indices[j]]];
                     i += sources[indMapping[indices[j]]].size();
-                } else {
-                    ans += s[i];
-                    i++;
                 }
                 j++;
             } else {
-                ans += s[i];
-                i++;
+                ans += s.substr(i, indices[j] - i);
+                i = indices[j];
             }
-        }
+        }
+        ans += s.substr(i, s.size() - i);
         return ans;
     }
 };
