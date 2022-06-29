f625bb10c6b9cdd04a07487fca2ab3bd90f39783
Ishu Dharmendra Garg <ishugarg567@gmail.com>
2022-06-29 16:51:26 +0530

    428 ms

--- solutions/373. Find K Pairs with Smallest Sums.cpp
+++ solutions/373. Find K Pairs with Smallest Sums.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         vector<vector<int>> ans;
-        k = min((long) k, (long) nums1.size() * (long) nums2.size());
+        // k = min((long) k, (long) nums1.size() * (long) nums2.size());
         auto compare = [&nums1, &nums2](pair<int, int> p1, pair<int, int> p2) {
           return (nums1[p1.first] +  nums2[p1.second]) > (nums1[p2.first] +  nums2[p2.second]);
         };
@@ -12,7 +12,7 @@ public:
         for(int i = 0; i < nums1.size(); i++) {
             pq.push({i, 0});
         }
-        while(k--) {
+        while(k-- and !pq.empty()) {
             auto p = pq.top();
             pq.pop();
             // cout << pq.size() << endl;
