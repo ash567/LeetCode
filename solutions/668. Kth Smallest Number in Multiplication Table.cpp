1e6b04059e2631ea2c960a5633a3d51280bad8d2
Ishu Dharmendra Garg <ishugarg567@gmail.com>
2022-06-29 12:41:07 +0530

    19 ms

---
+++ solutions/668. Kth Smallest Number in Multiplication Table.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    int getSmaller(int m, int n, int val) {
+        int count_ = 0;
+        for(int i = 1; i <= m; i++) {
+            count_ += min(n, val/i);
+        }
+        return count_;
+    }
+    
+public:
+    int findKthNumber(int m, int n, int k) {
+        int l = 1, r = m * n;
+        while(l < r) {
+            int mid = l + (r - l)/2;
+            if(getSmaller(m, n, mid) >= k) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
