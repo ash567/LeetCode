3e91af8dbbbea92c3bc13154c169c6e98ec2a3b4
Ishu Dharmendra Garg <ishugarg567@gmail.com>
2022-06-29 20:34:28 +0530

    9 ms

---
+++ solutions/786. K-th Smallest Prime Fraction.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    int countSmaller(vector<int>& arr, float r, int& a, int&b) {
+        float maxRatio = 0.0;
+        int j = 1, i = 0;
+        int count = 0;
+        // cout << "Curr Ratio: " << r << endl;
+        for(; j < arr.size(); j++) {
+            float currRatio = (float) arr[i] / (float) arr[j];
+            while(i < j and currRatio <= r) {
+                if(maxRatio < currRatio) {
+                    a = arr[i]; b = arr[j];
+                    maxRatio = currRatio;
+                }
+                i++;
+                currRatio = (float) arr[i] / (float) arr[j];
+            }
+            // cout << "j: " << j << ", i: " << i << endl;
+            count += i;
+        }
+        return count;
+    }
+    
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        float left = 0.0, right = 1.0, eps = 1e-9;
+        int a, b;
+        while(left < right) {
+            a = b = 1;
+            float mid = left + (right - left) / 2.0;
+            int counts = countSmaller(arr, mid, a, b);
+            // cout << mid << " " << counts << endl;
+            if(counts == k) {
+                break;
+            } else if(counts > k) {
+                right = mid;
+            } else {
+                left = mid + eps;
+            }
+        }
+        return vector<int>({a, b});
+    }
+};
